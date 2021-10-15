760c002b905061b7694e80de0642774d14022dc3
OneKolor <92573938+OneKolor@users.noreply.github.com>
2021-10-15 16:09:24 +0700

    Add files via upload

---
+++ Clickk4.js
@@ -0,0 +1,37 @@
+/*function Clickk4() {
+    var obj = document.getElementById('next-btn');
+    obj.onclick = P2;
+}
+function P2 () {
+    var obj = document.getElementById('it2')
+    obj.setAttribute('style','opacity:0%; transition: 2s')
+
+    var obj = document.getElementById('it3')
+    obj.setAttribute('style','opacity:100%; padding-left:65px; transition:2s')
+
+    var obj = document.getElementById('PAD04')
+    obj.setAttribute('style','transform: translate(-200px,0); opacity:0%; transition:1s')
+
+    var obj = document.getElementById('PAD05')
+    obj.setAttribute('style','transform: translate(-200px,0); opacity:0%; transition:2s')
+    
+    var obj = document.getElementById('PAD06')
+    obj.setAttribute('style','transform: translate(-200px,0); opacity:0%; transition:2.5s')
+
+    var obj = document.getElementById('PAD07')
+    obj.setAttribute('style','transform: translate(-200px,0); opacity:0%; transition:3s')
+
+    var obj = document.getElementById('PAD08')
+    obj.setAttribute('style','opacity:100%; translate(100px;0); transition:2s')
+
+    var obj = document.getElementById('PAD09')
+    obj.setAttribute('style','opacity:100%; transition:3s')
+    
+    var obj = document.getElementById('PAD10')
+    obj.setAttribute('style','opacity:100%; transition:3.5s')
+
+    var obj = document.getElementById('PAD11')
+    obj.setAttribute('style','opacity:100%; transition:4s')
+ 
+
+}*/
